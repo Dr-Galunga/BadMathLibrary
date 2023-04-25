0798bd5d31c30dc9cd9ece7db38f54c1a5697a01
Dr-Galunga <krck70153@gmail.com>
2023-04-25 00:12:52 -0400

    Didn't add much

--- factorial.c
+++ factorial.c
@@ -1,3 +1,7 @@
+/*------------------------------------------------------------------------------------------------------------------------------------------
+    Implements a basic factorial function using recursion. Probably not the most efficient but will help with the Sine and Cosine
+    functions in cosine.c and sine.c
+------------------------------------------------------------------------------------------------------------------------------------------*/
 #include "cmathl.h"
 
 double factorial(double n)
