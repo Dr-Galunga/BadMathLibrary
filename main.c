07b57c6259ea09e0ecad9b0ed618003062a7cb7c
Ciaran Krabak <ckr26@Ciarans-MacBook-Pro.local>
2023-04-25 00:59:16 -0400

    Added comment

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include "cml/cmathl.h"
 #include <stdio.h>
 
+//Basic functions right now but might want to add vector math later on
+//Could probably do this pretty easily for some of the easier functions like add or scalar multiplication
+//and even normalize but would be really slow with current cos and sin functions
+
 int main()
 {
     //This works right now
